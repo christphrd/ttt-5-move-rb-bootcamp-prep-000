5b117cc701da9eeed3fdf79fe9c505f6d3a94f09
Christopher Diep <diep.christopher@gmail.com>
2017-11-05 02:51:00 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,8 @@ def input_to_index(user_input)
 end
 
 def move(board, input, char = "X")
-  input_to_index(input)
-  def update_array_at_with(board, input, char)
-    board[input] = char
+  index = input_to_index(input)
+  def update_array_at_with(board, index, char)
+    board[index] = char
   end
 end
